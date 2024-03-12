953093aebb6b28ba7fe94ab00bbe06cfe7be4742
David Wallace <mypydavid@proton.me>
2024-03-12 23:04:43 +0100

    chore: make default_regions into dict of SpectrumRegionLimits

--- src/raman_fitting/models/deconvolution/spectrum_regions.py
+++ src/raman_fitting/models/deconvolution/spectrum_regions.py
@@ -1,24 +1,34 @@
 from enum import StrEnum
 from typing import Dict
 
-from pydantic import BaseModel
+from loguru import logger
+from pydantic import BaseModel, ValidationError
 from raman_fitting.config.default_models import load_config_from_toml_files
 
 
-def get_default_regions_from_toml_files() -> Dict[str, Dict[str, float]]:
-    default_regions = (
+class SpectrumRegionLimits(BaseModel):
+    name: str
+    min: int
+    max: int
+    extra_margin: int = 20
+
+
+def get_default_regions_from_toml_files() -> Dict[str, SpectrumRegionLimits]:
+    default_regions_from_file = (
         load_config_from_toml_files().get("spectrum", {}).get("regions", {})
     )
+    default_regions = {}
+    for region_name, region_limits in default_regions_from_file.items():
+        try:
+            valid_region = SpectrumRegionLimits(name=region_name, **region_limits)
+            default_regions[region_name] = valid_region
+        except ValidationError as e:
+            logger.error(f"Region definition for {region_name} is not valid: {e}")
     return default_regions
 
 
 RegionNames = StrEnum(
-    "RegionNames", " ".join(get_default_regions_from_toml_files()), module=__name__
+    "RegionNames",
+    " ".join(get_default_regions_from_toml_files().keys()),
+    module=__name__,
 )
-
-
-class SpectrumRegionLimits(BaseModel):
-    name: RegionNames
-    min: int
-    max: int
-    extra_margin: int = 20
