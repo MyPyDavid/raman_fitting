44b6acbf1bfd25253292085cb7c721e95b161d30
David Wallace <mypydavid@proton.me>
2025-02-02 22:55:34 +0100

    build: use bumpy-my-version for versioning
    Signed-off-by: David Wallace <mypydavid@proton.me>

--- src/raman_fitting/__init__.py
+++ src/raman_fitting/__init__.py
@@ -4,6 +4,7 @@ __status__ = "Development"
 __future_package_name__ = "pyramdeconv"
 __current_package_name__ = "raman_fitting"
 __package_name__ = __current_package_name__
+__version__ = "0.8.0"
 
 import sys
 from loguru import logger  # noqa: E402
